dca9fdd44b432a2ac24cc76a9f4403b69eb1b7e0
suraj-bv <surajbv5566@gamil.com>
2025-04-25 00:36:11 +0530

    Demo project

---
+++ react-project-demo/demo-project/src/assets/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div class='main-container'>
+      <h1>Navbar</h1>
+      <div class='container-two'>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Services</a>
+        <a href="">Account</a>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
