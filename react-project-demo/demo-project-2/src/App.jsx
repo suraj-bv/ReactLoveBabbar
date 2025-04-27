a274a971d514ad95fe0805322ad661c90795d32a
suraj-bv <surajbv5566@gamil.com>
2025-04-27 19:08:10 +0530

    Working with Bootstrap

---
+++ react-project-demo/demo-project-2/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Card from "./components/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
+
+export default App;
